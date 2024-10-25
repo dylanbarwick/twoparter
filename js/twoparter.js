7a0f0c9c3e3ed5032910dd9e3b3bf9ae3a9d090b
dylanbarwick <barwickconsultingltd@gmail.com>
2024-10-25 13:33:39 +0100

    [dev] - js timestamp in seconds, not milliseconds

--- js/twoparter.js
+++ js/twoparter.js
@@ -7,8 +7,8 @@
 
       $('.views-field-field-start-time-2 time').each(function() {
         let thisStartTime2 = $(this).text();
-        // Javascript timestamp.
-        let timestamp = new Date().getTime();
+        // Javascript timestamp in seconds.
+        let timestamp = Math.floor(new Date().getTime() / 1000);
         if (thisStartTime2 <= timestamp) {
           // Show the snippet2 etc.
           $(this).parents('.views-row').find('.views-field-field-snippet-2, .views-field-field-supporting-text-2').show();
